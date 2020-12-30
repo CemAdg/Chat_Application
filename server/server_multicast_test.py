6403fed633dd626265aff01618399f16ec952276
CemAdg <cem.akdag@hotmail.de>
2020-12-30 20:47:21 +0100

    updated multicast address from '224.3.29.71' to '224.0.0.1'

--- server/server_multicast_test.py
+++ server/server_multicast_test.py
@@ -26,7 +26,6 @@ def receiveMulticastMessage():
 def sendMulticastMessage():
 
     message = input("Enter message to multicast")
-
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     ttl = struct.pack('b', 1)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
