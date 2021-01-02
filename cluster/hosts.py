8a50421b5bc1a4f57a040d446bc5b97cede5c7c2
mshasanoglu <mshasanoglu@icloud.com>
2021-01-02 12:52:27 +0100

    Status 02.01.2020 new year changes

--- cluster/hosts.py
+++ cluster/hosts.py
@@ -5,10 +5,13 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.connect(("8.8.8.8", 80))
 myIP = sock.getsockname()[0]
+multicast = '224.3.29.71'
 
 leader = ''
-leader_crashed = False
-non_leader_crashed = False
+leader_crashed = ''
+non_leader_crashed = ''
+neighbour = ''
+network_changed = False
 server_list = []
 connections = []
 
