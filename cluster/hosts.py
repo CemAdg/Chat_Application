7e6329f6045727bf6ae58001c7609c35def673aa
mshasanoglu <mshasanoglu@icloud.com>
2021-01-08 16:50:49 +0100

    server comments DONE

--- cluster/hosts.py
+++ cluster/hosts.py
@@ -1,17 +1,26 @@
 # this is for all hosts
+
+# import modules
 import socket
 
-# Get own Machine IP
+# get own machine IP
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.connect(("8.8.8.8", 80))
 myIP = sock.getsockname()[0]
-multicast = '224.0.0.0'
 
+# connection variables
+buffer_size = 1024
+unicode = 'utf-8'
+
+# global IP variables
+multicast = '224.0.0.0'
 leader = ''
-leader_crashed = ''
-replica_crashed = ''
 neighbour = ''
-client_running = False
-network_changed = False
 server_list = []
 client_list = []
+
+# global state variables
+client_running = False
+network_changed = False
+leader_crashed = ''
+replica_crashed = ''
--- server.py
+++ server.py
@@ -1,32 +1,41 @@
 # this is a server
 
+# import Modules
 import socket
 import sys
 import threading
 import queue
 
 from time import sleep
-
 from cluster import hosts, ports, receive_multicast, send_multicast, heartbeat
 
-
+# Creating TCP Socket for Server
+# get the own IP from cluster.hosts
+# get the port used for Server from cluster.ports
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 host_address = (hosts.myIP, ports.server)
-buffer_size = 1024
-unicode = 'utf-8'
+
+# create First In First Out Queue
 FIFO = queue.Queue()
 
-
+# terminal printer for info
 def printer():
-    sleep(0.5)
     print(f'\n[SERVER] Server List: {hosts.server_list} ==> Leader: {hosts.leader}'
           f'\n[SERVER] Client List: {hosts.client_list}'
           f'\n[SERVER] Clients: {len(hosts.client_list)}'
           f'\n[SERVER] Neighbour ==> {hosts.neighbour}\n')
 
 
-def update_clients():
+# standardized for creating and starting Threads
+def new_thread(target, args):
+    t = threading.Thread(target=target, args=args)
+    t.daemon = True
+    t.start()
+
+
+# send all messages from FIFO Queue to all Clients
+def send_clients():
     message = ''
     while not FIFO.empty():
         message += f'{FIFO.get()}'
@@ -34,27 +43,30 @@ def update_clients():
 
     if message:
         for member in hosts.client_list:
-            member.send(message.encode(unicode))
+            member.send(message.encode(hosts.unicode))
 
 
+# handle all received messages from connected Clients
 def client_handler(client, address):
     while True:
         try:
-            data = client.recv(buffer_size)
+            data = client.recv(hosts.buffer_size)
+
+            # if client is disconnected or lost the connection
             if not data:
-                sleep(0.5)
                 print(f'{address} disconnected')
                 FIFO.put(f'\n{address} disconnected\n')
                 hosts.client_list.remove(client)
                 client.close()
                 break
-            FIFO.put(f'{address} said: {data.decode(unicode)}')
-            print(f'Message from {address} ==> {data.decode(unicode)}')
+            FIFO.put(f'{address} said: {data.decode(hosts.unicode)}')
+            print(f'Message from {address} ==> {data.decode(hosts.unicode)}')
         except Exception as e:
             print(e)
             break
 
 
+# bind the TCP Server Socket and listen for connections
 def start_binding():
     sock.bind(host_address)
     sock.listen()
@@ -64,10 +76,11 @@ def start_binding():
     while True:
         try:
             client, address = sock.accept()
+
+            # used just for Chat-Clients (filter out heartbeat)
             if address[0] not in hosts.server_list or address[0] == hosts.myIP:
                 print(f'{address} connected')
                 FIFO.put(f'\n{address} connected\n')
-                print(client)
                 hosts.client_list.append(client)
                 new_thread(client_handler, (client, address))
         except Exception as e:
@@ -75,38 +88,42 @@ def start_binding():
             break
 
 
-def new_thread(target, args):
-    t = threading.Thread(target=target, args=args)
-    t.daemon = True
-    t.start()
-
-
+# main Thread
 if __name__ == '__main__':
-    printer()
 
-    multicast_receiver = send_multicast.sending_request_to_multicast()
+    # trigger Multicast sender to check if a Multicast Receiver with given Multicast Address from cluster.hosts exists
+    multicast_receiver_exist = send_multicast.sending_request_to_multicast()
 
-    if not multicast_receiver:
+    # append the own IP to the Server List and assign the own IP as the Server Leader
+    if not multicast_receiver_exist:
         hosts.server_list.append(hosts.myIP)
         hosts.leader = hosts.myIP
+
+    # calling functions as Threads
     new_thread(receive_multicast.starting_multicast_receiver, ())
     new_thread(start_binding, ())
     new_thread(heartbeat.start_heartbeat, ())
 
+    # loop main Thread
     while True:
         try:
+            # send Multicast Message to all Multicast Receivers(Servers)
+            # used from Server Leader or if a Server Replica recognizes another Server Replica crash
             if hosts.leader == hosts.myIP and hosts.network_changed or hosts.replica_crashed:
                 if hosts.leader_crashed:
                     hosts.client_list = []
                 send_multicast.sending_request_to_multicast()
                 hosts.leader_crashed = False
-                hosts.replica_crashed = ''
                 hosts.network_changed = False
+                hosts.replica_crashed = ''
                 printer()
 
+            # used from Server Replica to set the variable to False
             if hosts.leader != hosts.myIP and hosts.network_changed:
                 hosts.network_changed = False
-            update_clients()
+
+            # function to send the FIFO Queue messages
+            send_clients()
         except KeyboardInterrupt:
             sock.close()
             print(f'\nClosing Server on IP {hosts.myIP} with PORT {ports.server}', file=sys.stderr)
